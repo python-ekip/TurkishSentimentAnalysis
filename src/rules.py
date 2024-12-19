7fa226c96d580f4b9961d6c5cd0f52ca57c76ed0
Fatih Parmaksız <fatihparm6@gmail.com>
2024-12-19 13:58:58 +0300

    new rules added

--- src/rules.py
+++ src/rules.py
@@ -14,9 +14,34 @@ class RULE():
         return False
 
     def fiildeOlumsuzlukEkiVar(self, analysis_results):
-        
         for single_analysis in analysis_results:
             if ":Neg" in single_analysis.format_string():
                 return True
         return False
     
+    def olumsuzFiilVar(self, analysis_results):
+        for single_analysis in analysis_results:
+            if ":Verb+Neg" in single_analysis.format_string(): 
+                return True
+        return False
+
+    def yetersizlikEkiVar(self, analysis_results):
+        for single_analysis in analysis_results:
+            if ":Unable" in single_analysis.format_string():
+                return True
+        return False
+    
+    def degismemPozitifMi(self, analysis_results):
+        """
+        'Değişmem' kelimesinin pozitif anlamda kullanılıp kullanılmadığını kontrol eder.
+        Args:
+            analysis_results (list): Zemberek'ten alınan analiz sonuçlarının listesi.
+        Returns:
+            bool: Pozitifse True, değilse False.
+        """
+        for i, analysis in enumerate(analysis_results):
+            # "değişmem" kelimesini kontrol et
+            if "değiş:Verb+me:Neg" in analysis.format_string():
+                if i > 0 and ":Dat" in analysis_results[i - 1].format_string():
+                    return True  # Pozitif
+        return False  # Negatif
